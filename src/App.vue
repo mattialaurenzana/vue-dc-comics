048a1ed5b3c2f700cb69303623d22a90c500449a
mattia laurenzana <mattilaure@gmail.com>
2022-01-27 16:48:36 +0100

    completamento header pagina

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <header-box />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import headerBox from './components/headerBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    headerBox
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import '@/style/main.scss';
+  
+  
 </style>
---
+++ src/components/headerBox.vue
@@ -0,0 +1,42 @@
+<template>
+    
+    <header>
+        <div class="header-left">
+            <img src="../assets/dc-logo.png" alt="">
+        </div>
+
+        <div class="header-right">
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+
+            </ul>
+
+        </div>
+    </header>
+
+</template>
+
+
+
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+    @import '../style/header.scss'
+</style>
